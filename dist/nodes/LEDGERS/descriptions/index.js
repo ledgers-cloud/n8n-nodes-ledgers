6fdacd46f6265b070341987ce73d39565b28f0d8
LEDGERS <devops@ledgers.cloud>
2025-06-06 10:57:09 +0530

    Delete dist directory

--- dist/nodes/LEDGERS/descriptions/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.contactOperations = void 0;
-const contactOperations_1 = require("./contactOperations");
-exports.contactOperations = contactOperations_1.contactOperations;
-//# sourceMappingURL=index.js.map
